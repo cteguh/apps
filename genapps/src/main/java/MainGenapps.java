1eba2c18042632703f715e99f57ace72d5e8a125
cteguh <y.christian.t.developer@gmail.com>
2011-10-14 20:23:31 +0700

    First Implementation
    Signed-off-by: cteguh <y.christian.t.developer@gmail.com>

---
+++ genapps/src/main/java/MainGenapps.java
@@ -0,0 +1,14 @@
+/**
+ * @project genapps
+ * @author Christian Teguh
+ * @created 14 Okt 2011 20.20.40
+ *
+ */
+
+public class MainGenapps {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Genapps");
+	}
+
+}
